21bd20a5d9bd4b26ecfce823d05174a960fa3cb7
mfilonenko <midnt.m@gmail.com>
2014-02-02 07:36:13 +0700

    add maven

--- musicBlink/src/main/java/com/midnight/musicblink/factory/WidgetFactory.java
+++ musicBlink/src/main/java/com/midnight/musicblink/factory/WidgetFactory.java
@@ -14,6 +14,7 @@ import com.midnight.musicblink.provider.WidgetProvider;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class WidgetFactory implements RemoteViewsService.RemoteViewsFactory {
 
     private Context context;
